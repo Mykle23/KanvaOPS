9660dff63ffb3dc36ac3e27046dea43e35c0cd1a
MykhayloB13M <mykle23.dev@gmail.com>
2024-08-23 11:21:19 +0200

    Frontend: Add styles to login

--- kanvaOps.front/src/app/pages/login/login-form/login-form.component.ts
+++ kanvaOps.front/src/app/pages/login/login-form/login-form.component.ts
@@ -5,8 +5,29 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+  styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
-
+  emailIsValid: boolean = false;
+  buttonTitle = 'Continuar';
+  checkEmail(
+    input: HTMLInputElement,
+    event: Event,
+    password: HTMLInputElement
+  ) {
+    event.preventDefault();
+    if (!input.value) return;
+    this.emailIsValid = input.validity.valid;
+    if (this.emailIsValid) {
+      this.buttonTitle = 'Iniciar Sesión';
+      password.className = 'password';
+      return;
+    } else {
+      password.className = 'hidden';
+      this.buttonTitle = 'Continuar';
+      password.value = '';
+    }
+    
+    console.log(this.emailIsValid);
+  }
 }
