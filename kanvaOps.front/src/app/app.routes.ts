0f763658f91f568baf2efe36754736c2226e58c4
MykhayloB13M <mykle23.dev@gmail.com>
2024-08-23 11:19:21 +0200

    Frontend: Add routes

--- kanvaOps.front/src/app/app.routes.ts
+++ kanvaOps.front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
