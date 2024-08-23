f228e64a53fe1e61a7214bca0c4de9807ee89d86
Alex <alejandro.marques.guerrero@gmail.com>
2024-08-23 18:32:20 +0200

    Created multiple components

--- kanvaOps.front/src/app/app.routes.ts
+++ kanvaOps.front/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes,RouterModule } from '@angular/router';
 import { LoginComponent } from './components/templates/login/login.component';
 import { NotFoundComponent } from './components/templates/not-found/not-found.component';
+import { BoardComponent } from './components/templates/board/board.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'board', component: BoardComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
