a5abe303ccffcfad983ece7ace99cfc7d983a38d
alex <alejandro.marques.guerrero@gmail.com>
2024-08-24 22:21:25 +0200

    editable board title

--- kanvaOps.front/src/app/components/organisms/board-header/board-header.component.ts
+++ kanvaOps.front/src/app/components/organisms/board-header/board-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-board-header',
@@ -8,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrl: './board-header.component.css'
 })
 export class BoardHeaderComponent {
+  @Input() boardTitle: string = '';
 
+  onTitleClick(titleComponent: HTMLHeadingElement, newTitle: HTMLInputElement) {
+    newTitle.style.width = `${this.boardTitle.length + 2}ch`
+
+    titleComponent.classList.add('hidden');
+    newTitle.classList.remove('hidden')
+    newTitle.focus();
+    newTitle.value = ''
+    newTitle.value = this.boardTitle
+    newTitle.select();
+  }
+
+  onTitleBlur(titleComponent: HTMLHeadingElement, newTitle: HTMLInputElement) {
+    if (!newTitle.value) {
+      newTitle.value = this.boardTitle;
+    }
+    newTitle.classList.add('hidden');
+    this.boardTitle = newTitle.value;
+    titleComponent.classList.remove('hidden');
+  }
+
+  autoSize = (element: HTMLInputElement) => {
+    element.style.width = '0';
+    const { borderLeftWidth, borderRightWidth } = getComputedStyle(element);
+
+    const numberPxToNumber = (numberPx: string) =>
+      parseInt(numberPx.slice(0, -2));
+    const borderWidth =
+      numberPxToNumber(borderLeftWidth) + numberPxToNumber(borderRightWidth);
+    // For some reason border is not included in scrollWidth
+    element.style.width = `${element.scrollWidth + borderWidth}px`;
+  };
 }
