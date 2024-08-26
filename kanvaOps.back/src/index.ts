4b75cf7590f14950340438096431e89e46322088
Matorry <rodri.martinceron@gmail.com>
2024-08-27 01:12:00 +0200

    Add db.connect, .env.example, and configuration for server port and database connection

--- kanvaOps.back/src/index.ts
+++ kanvaOps.back/src/index.ts
@@ -2,27 +2,31 @@ import createDebug from 'debug';
 import 'dotenv/config';
 import { createServer } from 'http';
 import { createApp, startApp } from './app.js';
+import { dbConnect } from './tools/db.conect.js';
 
 const debug = createDebug('KanvaOps:server');
 debug('Starting server');
 
-const port = process.env.PORT ?? 4200;
+const port = process.env.PORT ?? 7373;
 
 const app = createApp();
 const server = createServer(app);
 
-try {
+dbConnect().then(() => {
   startApp(app);
-  server.listen(port)
-} catch (error) {
-  server.emit("Error", error)
-}
+  server.listen(port);
+}).catch((error: Error) => {
+  debug('Database connection error:', error);
+  process.exit(1);
+});
 
 server.on("error", (error) => {
-  debug('Error:', error);
+  debug('Server error:', error);
   process.exit(1);
-})
+});
 
 server.on('listening', () => {
   debug(`Server express running on port ${port}`);
 });
+
+
