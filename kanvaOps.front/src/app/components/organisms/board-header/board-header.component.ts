106f1a08e9b2ec6f42c8a6657a3a6b6f0696f908
Alex <alejandro.marques.guerrero@gmail.com>
2024-08-26 18:22:30 +0200

    Board header layout

--- kanvaOps.front/src/app/components/organisms/board-header/board-header.component.ts
+++ kanvaOps.front/src/app/components/organisms/board-header/board-header.component.ts
@@ -1,23 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { SeparatorComponent } from "../../atoms/separator/separator.component";
 
 @Component({
   selector: 'app-board-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, SeparatorComponent],
   templateUrl: './board-header.component.html',
-  styleUrl: './board-header.component.css'
+  styleUrl: './board-header.component.css',
 })
 export class BoardHeaderComponent {
   @Input() boardTitle: string = '';
-
+  @Input() favourite: boolean = false;
   onTitleClick(titleComponent: HTMLHeadingElement, newTitle: HTMLInputElement) {
-    newTitle.style.width = `${this.boardTitle.length + 2}ch`
+    newTitle.style.width = `${this.boardTitle.length + 2}ch`;
 
     titleComponent.classList.add('hidden');
-    newTitle.classList.remove('hidden')
+    newTitle.classList.remove('hidden');
     newTitle.focus();
-    newTitle.value = ''
-    newTitle.value = this.boardTitle
+    newTitle.value = '';
+    newTitle.value = this.boardTitle;
     newTitle.select();
   }
 
@@ -40,5 +42,9 @@ export class BoardHeaderComponent {
       numberPxToNumber(borderLeftWidth) + numberPxToNumber(borderRightWidth);
     // For some reason border is not included in scrollWidth
     element.style.width = `${element.scrollWidth + borderWidth}px`;
+  };
+
+  onFavourite = () => {
+    this.favourite = !this.favourite;
   };
 }
